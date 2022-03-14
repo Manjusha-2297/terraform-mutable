ee8f1ea67c264cfbc6f53733cbba32eb44a074f1
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-14 12:31:46 +0530

    auto pushing chainges repo

--- vpc/peer.tf
+++ vpc/peer.tf
@@ -2,4 +2,5 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = "286176496219"
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = "vpc-0bad3c576d4bb924d"
+  auto_accept   = true
 }
