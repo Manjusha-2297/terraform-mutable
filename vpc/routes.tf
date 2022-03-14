7c52bf8e62812bb6d6ddeb615cd03be06b99bae8
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-14 23:18:21 +0530

    auto pushing chainges repo

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -17,3 +17,10 @@ resource "aws_route_table" "private" {
     Name = "private-rt-${var.env}-vpc"
   }
 }
+
+resource "aws_route" "public-peer-add" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = var.default_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  //depends_on                = [aws_route_table.testing]
+}
