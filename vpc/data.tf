fb0cd20f73291535b9b839cbafae0b8b1e1a3e8b
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-14 23:35:24 +0530

    auto pushing chainges repo

--- vpc/data.tf
+++ vpc/data.tf
@@ -3,3 +3,11 @@ data "aws_caller_identity" "current" {}
 #output "account_id" {
 #  value = data.aws_caller_identity.current.account_id
 #}
+
+data "aws_vpc" "default" {
+  id = var.default_vpc
+}
+
+output "vpc" {
+  value = data.aws_vpc.default
+}
