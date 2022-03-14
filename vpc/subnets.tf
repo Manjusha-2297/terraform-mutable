ab2fa38dd8063a65fd49b73b9138a5c0b58b2ee3
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-14 11:32:14 +0530

    auto pushing chainges repo

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.public_subnet_cidr, count.index)
+  availability_zone = element(var.AZS, count.index)
 
   tags = {
     Name = "public-subnet-${count.index + 1}"
@@ -12,6 +13,7 @@ resource "aws_subnet" "private" {
   count = length(var.private_subnet_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.private_subnet_cidr, count.index)
+  availability_zone = element(var.AZS, count.index)
 
   tags = {
     Name = "private-subnet-${count.index + 1}"
