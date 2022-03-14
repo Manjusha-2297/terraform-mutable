8c20a91c471170a971199a899a26fd17f8fe960f
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-14 11:28:35 +0530

    auto pushing chainges repo

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -7,3 +7,13 @@ resource "aws_subnet" "public" {
     Name = "public-subnet-${count.index + 1}"
   }
 }
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.private_subnet_cidr, count.index)
+
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
