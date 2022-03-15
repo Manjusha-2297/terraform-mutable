73ffd2398c91989e536b672b91bfef84fbd23b1c
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-15 12:22:33 +0530

    auto pushing chainges repo

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -1,7 +1,23 @@
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route = []
+  route = [
+    {
+      cidr_block                 = var.default_vpc_cidr
+      egress_only_gateway_id     = ""
+      gateway_id                 = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      nat_gateway_id             = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_peering_connection_id  = aws_vpc_peering_connection.peer.id
+      carrier_gateway_id         = ""
+      destination_prefix_list_id = ""
+      local_gateway_id           = ""
+      vpc_endpoint_id            = ""
+    }
+  ]
 
   tags = {
     Name = "public-rt-${var.env}-vpc"
@@ -11,26 +27,29 @@ resource "aws_route_table" "public" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route = []
+  route = [
+    {
+      cidr_block                 = var.default_vpc_cidr
+      egress_only_gateway_id     = ""
+      gateway_id                 = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      nat_gateway_id             = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_peering_connection_id  = aws_vpc_peering_connection.peer.id
+      carrier_gateway_id         = ""
+      destination_prefix_list_id = ""
+      local_gateway_id           = ""
+      vpc_endpoint_id            = ""
+    }
+  ]
 
   tags = {
     Name = "private-rt-${var.env}-vpc"
   }
 }
 
-resource "aws_route" "public-peer-add" {
-  route_table_id            = aws_route_table.public.id
-  destination_cidr_block    = var.default_vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-  //depends_on                = [aws_route_table.testing]
-}
-
-resource "aws_route" "private-peer-add" {
-  route_table_id            = aws_route_table.private.id
-  destination_cidr_block    = var.default_vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-  //depends_on                = [aws_route_table.testing]
-}
 
 resource "aws_route_table_association" "public" {
   count = length(aws_subnet.public.*.id)
@@ -44,10 +63,10 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private.id
 }
 
-resource "aws_route" "default-vpc-route-table" {
-  count = length(local.all_cidr_vpc)
-  route_table_id            = data.aws_vpc.default.main_route_table_id
-  destination_cidr_block    = element(local.all_cidr_vpc, count.index)
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-  //depends_on                = [aws_route_table.testing]
-}
+#resource "aws_route" "default-vpc-route-table" {
+#  count = length(local.all_cidr_vpc)
+#  route_table_id            = data.aws_vpc.default.main_route_table_id
+#  destination_cidr_block    = element(local.all_cidr_vpc, count.index)
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+#  //depends_on                = [aws_route_table.testing]
+#}
