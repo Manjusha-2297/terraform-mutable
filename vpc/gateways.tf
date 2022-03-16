714425f09802af9d251707745a5ca1382f2e9a27
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-16 20:48:26 +0530

    auto pushing chainges repo

--- vpc/gateways.tf
+++ vpc/gateways.tf
@@ -5,3 +5,18 @@ resource "aws_internet_gateway" "gw" {
     Name = "${var.env}-igw"
   }
 }
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = aws_subnet.public.*.id[0]
+
+  tags = {
+    Name = "${var.env}-natgw"
+  }
+
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_eip" "ngw" {
+  vpc      = true
+}
