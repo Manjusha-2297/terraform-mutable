4a7eb8f8d01daa69196ba9f308616bc649719a0c
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-16 17:33:16 +0530

    auto pushing chainges repo

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "public" {
 
   route = [
     {
-      cidr_block                 = [ var.default_vpc_cidr ]
+      cidr_block                 = var.default_vpc_cidr
       egress_only_gateway_id     = ""
       gateway_id                 = ""
       instance_id                = ""
@@ -29,20 +29,20 @@ resource "aws_route_table" "private" {
 
   route = [
     {
-    cidr_block                 = [ var.default_vpc_cidr ]
-    egress_only_gateway_id     = ""
-    gateway_id                 = ""
-    instance_id                = ""
-    ipv6_cidr_block            = ""
-    nat_gateway_id             = ""
-    network_interface_id       = ""
-    transit_gateway_id         = ""
-    vpc_peering_connection_id  = aws_vpc_peering_connection.peer.id
-    carrier_gateway_id         = ""
-    destination_prefix_list_id = ""
-    local_gateway_id           = ""
-    vpc_endpoint_id            = ""
-  }
+      cidr_block                 = var.default_vpc_cidr
+      egress_only_gateway_id     = ""
+      gateway_id                 = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      nat_gateway_id             = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_peering_connection_id  = aws_vpc_peering_connection.peer.id
+      carrier_gateway_id         = ""
+      destination_prefix_list_id = ""
+      local_gateway_id           = ""
+      vpc_endpoint_id            = ""
+    }
   ]
 
   tags = {
