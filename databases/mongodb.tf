8b73ab0d5d539f66489d1e43f74633afbd5c896c
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-17 11:44:33 +0530

    auto pushing chainges repo

--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -2,6 +2,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   ami           = data.aws_ami.ami
   instance_type = var.mongodb_instance_type
 
+
   tags = {
     Name = "mongodb-${var.env}"
   }
