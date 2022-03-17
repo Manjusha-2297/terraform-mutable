291d832dc3e8227c9c04d188e01211c1ff11fc15
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-17 12:05:55 +0530

    auto pushing chainges repo

--- databases/data.tf
+++ databases/data.tf
@@ -8,7 +8,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
     bucket = "terraform-2297"
-    key    = "terraform-mutable/databases/${var.env}/terraform.tfstate"
+    key    = "terraform-mutable/vpc/${var.env}/terraform.tfstate"
     region = "us-east-1"
    }
 }
