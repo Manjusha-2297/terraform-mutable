9f5cd90ccc0e886d0a074e34f51ef92e5d3c4bb7
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-17 12:21:42 +0530

    auto pushing chainges repo

--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -2,6 +2,7 @@ resource "aws_spot_instance_request" "mongodb" {
   ami           = data.aws_ami.ami.id
   instance_type = var.mongodb_instance_type
   subnet_id = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS[0]
+  vpc_security_group_ids = [aws_security_group.allow_mongodb.id]
 
   tags = {
     Name = "mongodb-${var.env}"
@@ -9,7 +10,42 @@ resource "aws_spot_instance_request" "mongodb" {
 }
 
 resource "aws_ec2_tag" "mongodb" {
-  key         = "Name"
   resource_id = aws_spot_instance_request.mongodb.spot_instance_id
+  key         = "Name"
   value       = "mongodb-${var.env}"
+}
+
+resource "aws_security_group" "allow_mongodb" {
+  name        = "allow_mongodb_${var.env}"
+  description = "Allow Mongodb"
+
+  ingress = [{
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+    ipv6_cidr_blocks = []
+  },
+  {
+    description      = "MONGODB"
+    from_port        = 27017
+    to_port          = 27017
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+    ipv6_cidr_blocks = []
+}
+
+    ]
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+  }
+
+  tags = {
+    Name = "allow_mongodb_${var.env}"
+  }
 }
