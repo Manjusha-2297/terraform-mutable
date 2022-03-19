19ba52701d56438bd2c3d08d876f36b7fdafb330
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-19 17:38:45 +0530

    push make

---
+++ app-module/app-deploy.tf
@@ -0,0 +1,18 @@
+resource "null_resource" "app-deploy" {
+  count = length(local.all_instance_ip)
+  triggers = {
+    abc = timestamp()
+  }
+  provisioner "remote-exec" {
+    connection {
+      host     = element(local.all_instance_ip, count.index)
+      user     = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["ssh_user"]
+      password = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["ssh_password"]
+    }
+
+    inline = [
+
+      "ansible-pull -i localhost, -U https://github.com/Manjusha-2297/ansible.git roboshop-pull.yml -e COMPONENT=${var.component}"
+    ]
+  }
+}
--- app-module/locals.tf
+++ app-module/locals.tf
@@ -1,4 +1,5 @@
 locals {
   default_vpc_cidr = tolist([data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR,""])
   all_cidr_vpc = compact(concat(data.terraform_remote_state.vpc.outputs.VPC_CIDR, local.default_vpc_cidr))
+  all_instance_ip = concat(aws_instance.on-demand.*.private_ip,aws_spot_instance_request.spot.*.private_ip)
 }
