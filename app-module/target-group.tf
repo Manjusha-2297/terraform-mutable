c8583e91862d4532db2a286f4eeb1a4405e633c7
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-20 18:28:18 +0530

    push make

--- app-module/target-group.tf
+++ app-module/target-group.tf
@@ -1,6 +1,14 @@
-resource "aws_lb_target_group" "test" {
+resource "aws_lb_target_group" "target-group" {
   name     = "${var.component}-${var.env}-tg"
   port     = var.port
   protocol = "HTTP"
   vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
+  health_check {
+    enabled = true
+    healthy_threshold = 2
+    interval = 5
+    path = "/health"
+    port = var.port
+    unhealthy_threshold = 2
+  }
 }
