934d0a6717e22d111399f624dfe78230d4fe5e1c
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-20 22:16:36 +0530

    push make

--- app-module/data.tf
+++ app-module/data.tf
@@ -17,7 +17,7 @@ data "terraform_remote_state" "alb" {
   backend = "s3"
   config = {
     bucket = "terraform-2297"
-    key    = "terraform-mutable/vpc/${var.env}/terraform.tfstate"
+    key    = "terraform-mutable/alb/${var.env}/terraform.tfstate"
     region = "us-east-1"
   }
 }
